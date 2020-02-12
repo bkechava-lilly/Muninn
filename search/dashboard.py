ab2fdcc40ffb9746d64d049f9551730f8ab1445b
C180489 <kechavarzi_bobak_d@lilly.com>
2020-02-12 11:40:08 -0800

    Pushed from Domino: https://domino.am.lilly.com/u/C180489/Munnin/runs/5e44463e4cedfd000747b21c

--- search/dashboard.py
+++ search/dashboard.py
@@ -210,4 +210,4 @@ def render_markdown(rows, selected_row_indices):
 
 if __name__ == '__main__':
     # Startup the server
-    APP.run_server(debug=True, host='0.0.0.0',port=8050)
+    APP.run_server(debug=True, host='0.0.0.0',port=8888)
